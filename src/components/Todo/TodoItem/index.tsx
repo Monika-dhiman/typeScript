a07a3a22e94039c8e9ecadc8cd8fd7059195e489
Monika Dhiman <monidhiman222@gmail.com>
2023-08-11 18:17:49 +0530

    first commit

---
+++ src/components/Todo/TodoItem/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoItem: React.FC = () => {
+  return (
+    <>
+      <h3>Code</h3>
+    </>
+  );
+};
+export default TodoItem;
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+const Todo: React.FC = () => {
+  return (
+    <>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      
+      
+    </>
+  );
+};
+export default Todo;
